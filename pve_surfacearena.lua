8bd5ad438c31267987e632f2b179906c7e1676f0
Centralan <Jessegauthier070296@gmail.com>
2023-09-12 14:36:32 -0400

    Update pve_surfacearena.lua

--- pve_surfacearena.lua
+++ pve_surfacearena.lua
@@ -533,6 +533,7 @@ registerHook("INTERACT", "start_r5", 143, "mobarena", -7.0, 66.0, -3.0);
 --Rewards----------
 ------------------------------------------------------
 
+local world = World:new('mobarena');
 local R1Chest = Location:new(world, -52.0, 114.0, 9.0);
 local ChestPlayers = {};
 local player = Player:new(data.player);
