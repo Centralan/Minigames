702c566e496b6a1e39ed0bc50f9857cc9aa8495a
Centralan <Jessegauthier070296@gmail.com>
2023-09-20 15:19:04 -0400

    Starting Nether Lua, Testing Catch

--- pve_nether.lua
+++ pve_nether.lua
@@ -1,1 +1,17 @@
-Creation of file
+local myWorld = World:new('mobarena_nether');
+local oldWorld = World:new('mobarena');
+local nethersound = Location:new(myWorld, 0.0, 86.0, 0.0);
+
+----------------
+--Arena Catch --
+----------------
+
+local myWorld = World:new('Code4');
+local nethercatch = Location:new(myWorld, 0, 86.0, 0.0);
+
+function nether_catch(data)
+       local targetPlayer = Player:new(data.player);
+       targetPlayer:teleport(location);
+end
+
+registerHook("REGION_ENTER", "nether_catch", "mobarena_nether-pve_nether_catch");
