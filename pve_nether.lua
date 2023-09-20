f04d37ccd1848dd87d21dafde380a6245fb18cbd
Centralan <Jessegauthier070296@gmail.com>
2023-09-20 15:24:04 -0400

    added nether catch

--- pve_nether.lua
+++ pve_nether.lua
@@ -1,13 +1,20 @@
+---------------
+--local World--
+---------------
+
 local myWorld = World:new('mobarena_nether');
 local oldWorld = World:new('mobarena');
 local nethersound = Location:new(myWorld, 0.0, 86.0, 0.0);
 
+-------------
+--local tps--
+-------------
+
+local nethercatch = Location:new(myWorld, 0, 86.0, 0.0);
+
 ----------------
 --Arena Catch --
 ----------------
-
-local myWorld = World:new('Code4');
-local nethercatch = Location:new(myWorld, 0, 86.0, 0.0);
 
 function nether_catch(data)
        local targetPlayer = Player:new(data.player);
