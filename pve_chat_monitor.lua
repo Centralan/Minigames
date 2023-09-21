4b6db4a1d3048cb2a996ffd40e80b62ae5f33749
Centralan <Jessegauthier070296@gmail.com>
2023-09-21 16:46:32 -0400

    cancel rounds

--- pve_chat_monitor.lua
+++ pve_chat_monitor.lua
@@ -17,6 +17,11 @@ function chatMonitor(data)
 
 		if hasPrefix(message, "#ResetNether") then
 			local playerName = splitPlayerName(message, 16);
+			nR1:cancel()
+			nR2:cancel()
+			nR3:cancel()
+			nR4:cancel()
+			nR5:cancel()
 			nR1Done = false;
                         nR2Done = false;
                         nR3Done = false;
@@ -27,17 +32,25 @@ function chatMonitor(data)
 
 		if hasPrefix(message, "#ResetMine") then
 			local playerName = splitPlayerName(message, 16);
+			mR1:cancel()
+			mR2:cancel()
+			mR3:cancel()
+			mR4:cancel()
 			mR1Done = false;
                         mR2Done = false;
                         mR3Done = false;
                         mR4Done = false;
-			mR5Done = false;
 	                mRoundRunning = false;
 			player:sendMessage("Reseting &6Mine &fArena.");
 
 
 		if hasPrefix(message, "#ResetSurface") then
 			local playerName = splitPlayerName(message, 16);
+			sR1:cancel()
+			sR2:cancel()
+			sR3:cancel()
+			sR4:cancel()
+			sR5:cancel()
 			sR1Done = false;
                         sR2Done = false;
                         sR3Done = false;
