7a5bc87dad779586b72cc21c78d8bb0ef82a95fa
Centralan <Jessegauthier070296@gmail.com>
2023-09-20 22:06:28 -0400

    added worlds to chat moniter

--- pve_chat_monitor.lua
+++ pve_chat_monitor.lua
@@ -1,5 +1,33 @@
+local myWorld = World:new('mobarena'); 
+local myWorld5 = World:new('mobarena_nether');
+
 function chatMonitor(data)
 	-- Make sure it's you giving the command.
-	if data.player == "Centralan" or data.player == "iliketoeatpenuts" or data.player == "Kruithne" then
+	if data.player == "Centralan"
 		local player = Player:new(data.player);
 		local message = data.message;
+
+		if hasPrefix(message, "#ResetNether") then
+			local playerName = splitPlayerName(message, 16);
+			addPlayerToGreenTeam(playerName);
+			player:sendMessage("Reseting &6Nether &fArena.");
+
+		if hasPrefix(message, "#ResetMine") then
+			local playerName = splitPlayerName(message, 16);
+			addPlayerToGreenTeam(playerName);
+			player:sendMessage("Reseting &6Mine &fArena.");
+
+
+		if hasPrefix(message, "#ResetSurface") then
+			local playerName = splitPlayerName(message, 16);
+			addPlayerToGreenTeam(playerName);
+			player:sendMessage("Reseting &6Surface &fArena.");
+
+
+				end
+			end
+		end
+
+	registerHook("CHAT_MESSAGE", "chatMonitor", "mobarena");
+	registerHook("CHAT_MESSAGE", "chatMonitor", "mobarena_nether");
+
