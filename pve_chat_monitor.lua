9bfed3ae223163ad312fd52835fc488c093f3a25
Centralan <Jessegauthier070296@gmail.com>
2023-09-20 22:14:07 -0400

    added support for reseting rounds

--- pve_chat_monitor.lua
+++ pve_chat_monitor.lua
@@ -9,18 +9,33 @@ function chatMonitor(data)
 
 		if hasPrefix(message, "#ResetNether") then
 			local playerName = splitPlayerName(message, 16);
-			addPlayerToGreenTeam(playerName);
+			nR1Done = false;
+                        nR2Done = false;
+                        nR3Done = false;
+                        nR4Done = false;
+			nR5Done = false;
+	                nRoundRunning = false;
 			player:sendMessage("Reseting &6Nether &fArena.");
 
 		if hasPrefix(message, "#ResetMine") then
 			local playerName = splitPlayerName(message, 16);
-			addPlayerToGreenTeam(playerName);
+			mR1Done = false;
+                        mR2Done = false;
+                        mR3Done = false;
+                        mR4Done = false;
+			mR5Done = false;
+	                mRoundRunning = false;
 			player:sendMessage("Reseting &6Mine &fArena.");
 
 
 		if hasPrefix(message, "#ResetSurface") then
 			local playerName = splitPlayerName(message, 16);
-			addPlayerToGreenTeam(playerName);
+			sR1Done = false;
+                        sR2Done = false;
+                        sR3Done = false;
+                        sR4Done = false;
+			sR5Done = false;
+	                sRoundRunning = false;
 			player:sendMessage("Reseting &6Surface &fArena.");
 
 
