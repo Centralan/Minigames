aa8fd79e0a312e4d65994b1aad5bbd96f65f70b1
Centralan <Jessegauthier070296@gmail.com>
2024-01-19 20:18:38 -0500

    Update treearena.lua

--- PvP/treearena.lua
+++ PvP/treearena.lua
@@ -1,10 +1,10 @@
 local world = World:new('pvparena');
-local pvpcompass = Location:new(world, -1030.0, 22.0, 446.0);
 
 function respawn_wipe(data)
        local player = Player:new(data.player);
              player:clearInventory();
-             pvpcompass:cloneChestToPlayer(player.name);
+             pvpcompasschest:cloneChestToPlayer(player.name);
 end
 
 registerHook("REGION_LEAVE", "respawn_wipe", "pvparena-pvp_wipe")
+
